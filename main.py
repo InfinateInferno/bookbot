902949e76354dfdf853187899681298872c9f5b6
InfinateInferno <ben.thirlaway1@gmail.com>
2026-01-05 21:33:16 +1000

    Modified main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+path = "books/frankenstein.txt"
+def get_book_text(book):
+    with open(book) as f:
+        temp_file = f.read()
+    return temp_file
+def main():
+    text = get_book_text(path)
+    print(text)
+main()
