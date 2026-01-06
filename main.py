daa2e88df5d9990a0241f88113bf5da81cd762a6
InfinateInferno <ben.thirlaway1@gmail.com>
2026-01-06 21:43:35 +1000

    modified main.py

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ def get_book_text(book):
     with open(book) as f:
         temp_file = f.read()
     return temp_file
+def word_count(book):
+    text = get_book_text(book)
+    individual_words = text.split()
+    number = len(individual_words)
+    return number
 def main():
-    text = get_book_text(path)
-    print(text)
+    number_of_words = word_count(path)
+    result = f"Found {number_of_words} total words"
+    print(result)
 main()
